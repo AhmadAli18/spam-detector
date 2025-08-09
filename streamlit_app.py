f8cbfc0dc261e0d2cc675a53bcfa66927a2edfd7
AhmadAli18 <aliahmad33k@gmail.com>
2025-08-09 13:38:48 +0530

    local environment par run ho rha

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import tensorflow as tf
 import pickle
 import string
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.sequence import pad_sequences
 from nltk.corpus import stopwords
 import nltk
 
